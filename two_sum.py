34ae58d69d9d02b2a38f664db2dea21a74d6c478
shreeja10 <shreejasinha24@gmail.com>
2026-08-31 11:00:54 +0530

    feat:two sum

--- two_sum.py
+++ two_sum.py
@@ -1,6 +1,11 @@
-arr=[2,7,11,15]
-target=9
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        if arr[i]+arr[j]==target:
-            print(i,j)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return(i,j)
