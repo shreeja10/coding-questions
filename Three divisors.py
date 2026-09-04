66c9be7401468f091c27305de28f40e52681451e
shreeja10 <shreejasinha24@gmail.com>
2026-09-04 17:24:26 +0530

    feat: Check if the number has exactly three divisors or not

---
+++ Three divisors.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        count=0
+        for i in range(1,n+1):
+            if n%i==0:
+                count+=1
+        if count==3:
+            return True
+        else:
+            return False
