97fc4aba5d67339e001cb4310f55277e8ea685b6
shreeja10 <shreejasinha24@gmail.com>
2026-09-08 22:04:29 +0530

    feat:Find single number

---
+++ Single Number 2.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for keys,values in d.items():
+            if values==1:
+                return keys
