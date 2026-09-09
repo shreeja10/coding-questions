9f586854a585a8e7e7861d3c5e4e7daf6b0ebb41
shreeja10 <shreejasinha24@gmail.com>
2026-09-09 14:42:37 +0530

    feat:Find x power n

---
+++ Power(x,n).py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if x>0 or x<0 or n>0:
+            return x**n
